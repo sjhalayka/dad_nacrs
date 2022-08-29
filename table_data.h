8b94bbe5fcd8a2a995affd61bff061a6ff2f3b28
Shawn Halayka <sjhalayka@gmail.com>
2022-08-28 20:24:16 -0600

    Add files via upload

--- table_data.h
+++ table_data.h
@@ -22,13 +22,14 @@ protected:
 	size_t neutropenia_index = 0;
 	vector<size_t> diag_codes;
 
+	// The following are helper functions that are inaccessible
+	// from outside of the class
+
 	// This function *must* be implemented by inheriting classes
 	// Making it equal to 0 means that one cannot instantiate the
 	// base class table_data
 	virtual bool get_various_column_indices(void) = 0;
 
-	// These are helper functions that are inaccessible
-	// from outside of the class
 	size_t get_index(const string& column_name);
 	bool get_data_buffer(const string& filename);
 	bool get_data_line_by_line(const string& filename);
