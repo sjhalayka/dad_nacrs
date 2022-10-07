ac2353cdae678d89e472f78dc4def713a282212c
Shawn Halayka <sjhalayka@gmail.com>
2022-10-07 10:26:39 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -14,28 +14,39 @@ int main(void)
 	diagnosis_indicator d;
 
 	d.diagnosis_name = "Neutropenia_Indicator";
-	d.diagnosis_codes.push_back("D700");
+	d.diagnosis_codes = { "D700" };
 	indicators.push_back(d);
 
 	d.diagnosis_name = "Myocarditis_Indicator";
-	d.diagnosis_codes.clear();
-	d.diagnosis_codes.push_back("I401");
-	d.diagnosis_codes.push_back("I408");
-	d.diagnosis_codes.push_back("I409");
-	d.diagnosis_codes.push_back("I41");
-	d.diagnosis_codes.push_back("I514");
+	d.diagnosis_codes = { "I401", "I408", "I409", "I41", "I514" };
 	indicators.push_back(d);
 
 	d.diagnosis_name = "Cardiomyopathy_Indicator";
-	d.diagnosis_codes.clear();
-	d.diagnosis_codes.push_back("I420");
-	d.diagnosis_codes.push_back("I421");
-	d.diagnosis_codes.push_back("I423");
-	d.diagnosis_codes.push_back("I424");
-	d.diagnosis_codes.push_back("I425");
-	d.diagnosis_codes.push_back("I427");
-	d.diagnosis_codes.push_back("I428");
-	d.diagnosis_codes.push_back("I429");
+	d.diagnosis_codes = { "I420", "I421", "I423" "I424", "I425", "I427", "I428", "I429" };
+	indicators.push_back(d);
+
+	d.diagnosis_name = "Schizophrenia_Indicator";
+	d.diagnosis_codes = { "F200", "F201", "F202", "F203", "F204", "F205", "F206", "F208", "F209" };
+	indicators.push_back(d);
+
+	d.diagnosis_name = "Schizoaffective_Indicator";
+	d.diagnosis_codes = { "F250", "F251", "F258", "F259" };
+	indicators.push_back(d);
+	
+	d.diagnosis_name = "Bipolar_Indicator";
+	d.diagnosis_codes = { "F310", "F311", "F312", "F313", "F314", "F315", "F316", "F317", "F318", "F319" };
+	indicators.push_back(d);
+
+	d.diagnosis_name = "Other_non-organic_psychosis_Indicator";
+	d.diagnosis_codes = { "F28" };
+	indicators.push_back(d);
+
+	d.diagnosis_name = "Unspecified_organic_psychosis_Indicator";
+	d.diagnosis_codes = { "F29" };
+	indicators.push_back(d);
+	
+	d.diagnosis_name = "Self-harm_Indicator";
+	d.diagnosis_codes = { "X60", "X61", "X62", "X63", "X64", "X65", "X66", "X67", "X68", "X69" "X70", "X71", "X72", "X73", "X74", "X75", "X76", "X77", "X78", "X79", "X80", "X81", "X82", "X83", "X84" };
 	indicators.push_back(d);
 
 
@@ -50,6 +61,15 @@ int main(void)
 	cout << "Neutropenia count:    " << dtd.get_count("Neutropenia_Indicator") << endl;
 	cout << "Myocarditis count:    " << dtd.get_count("Myocarditis_Indicator") << endl;
 	cout << "Cardiomyopathy count: " << dtd.get_count("Cardiomyopathy_Indicator") << endl;
+
+	cout << "Schizophrenia count: " << dtd.get_count("Schizophrenia_Indicator") << endl;
+	cout << "Schizoaffective count: " << dtd.get_count("Schizoaffective_Indicator") << endl;
+
+	cout << "Bipolar count: " << dtd.get_count("Bipolar_Indicator") << endl;
+	cout << "Other non-organic psychosis count: " << dtd.get_count("Other_non-organic_psychosis_Indicator") << endl;
+	cout << "Unspecified organic psychosis count: " << dtd.get_count("Unspecified_organic_psychosis_Indicator") << endl;
+	cout << "Self-harm: " << dtd.get_count("Self-harm_Indicator") << endl;
+
 	cout << endl;
 
 	if (false == dtd.save_to_CSV_buffer("Z:/Smartphone_2/Shawn/Indicators/dad_cohorts_08_18_with_indicators.csv"))
